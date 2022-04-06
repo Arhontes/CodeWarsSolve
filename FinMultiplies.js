6b593f61d791a6ba27d1e93022c3c7918a145981
Arhontes <arhontes@yandex.ru>
2022-04-06 14:27:27 +0300

    Added new function 8 kiy CodeWars. return list of multiplies integer

---
+++ FinMultiplies.js
@@ -0,0 +1,13 @@
+/*It's the that return list of multiplies integer to limit inclusive*/
+function findMultiples(integer, limit) {
+    let array = [];
+    let number = integer
+    while(number<=limit){
+        array.push(number)
+        number = number+integer
+    }
+    return array
+}
+function findMultiples2(int,limit){
+    return Array(Math.floor(limit/int)).fill(1).map((x,i)=>int*(i+1));
+}
